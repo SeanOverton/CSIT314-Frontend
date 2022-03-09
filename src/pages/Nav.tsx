662709679a871882cac4c5453e4e6e3bb1e89416
Sean Overton <sean14o8.so@gmail.com>
2022-03-09 21:15:25 +1100

    Added a simple burger menu

--- src/pages/Nav.tsx
+++ src/pages/Nav.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import './navbar.css';
+import Navbar from "react-bootstrap";
+import { slide as Menu } from 'react-burger-menu';
 
 
 const Nav = () => {
@@ -8,13 +10,24 @@ const Nav = () => {
     // let userType = props.userType;
     
     return (
-        <div className="navbar">
-            <Link className="navbar-link" to="/">Home</Link>
-            <Link className="navbar-link" to="/login">Login</Link>
-            <Link className="navbar-link" to="/signup">Sign Up</Link>
-            <Link className="navbar-link" to="/request">Request our service</Link>
-            <Link className="navbar-link" to="/requests">View all requests</Link>
-        </div>
+        <>
+            <div className="mobile-hide">
+                <Link className="navbar-link" to="/">Home</Link>
+                <Link className="navbar-link" to="/login">Login</Link>
+                <Link className="navbar-link" to="/signup">Sign Up</Link>
+                <Link className="navbar-link" to="/request">Request our service</Link>
+                <Link className="navbar-link" to="/requests">View all requests</Link>
+            </div>
+            <div className="mobile-show"> 
+                <Menu>
+                    <Link className="menu-item" to="/">Home</Link>
+                    <Link className="menu-item" to="/login">Login</Link>
+                    <Link className="menu-item" to="/signup">Sign Up</Link>
+                    <Link className="menu-item" to="/request">Request our service</Link>
+                    <Link className="menu-item" to="/requests">View all requests</Link>
+                </Menu>
+            </div>
+        </>
     );
 };
 
