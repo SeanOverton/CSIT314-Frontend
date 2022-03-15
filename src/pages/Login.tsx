21aa923e5f7ea16191253a903960f038e5806b42
Sean Overton <sean14o8.so@gmail.com>
2022-03-15 16:18:12 +1100

    bugfix/upon login state would not update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,15 +3,18 @@ import React, { Component } from "react";
 import Nav from "./Nav";
 import Footer from './Footer';
 import auth from "./Auth";
+import { Navigate } from "react-router-dom";
 
 import "./forms.css";
 
 const LogIn = (props: any) => {
     return (
         <>
+        {!auth.isAuthenticated() ? (
+            <>
             <Nav/>
             <div className="auth-inner">
-                {/* <form> */}
+                <form>
                     <h3>Login</h3>
 
                     <div className="form-group">
@@ -45,9 +48,13 @@ const LogIn = (props: any) => {
                     <p className="forgot-password text-right">
                         Forgot <a href="#">password?</a>
                     </p>
-                {/* </form> */}
+                </form>
             </div>
             <Footer/>
+            </>
+        ) : (
+            <Navigate to="/" replace />
+        )}
         </>
     );
 };
