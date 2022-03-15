c0151a1f8201af76ffdb5abb811cae9fc9e6499b
Sean Overton <sean14o8.so@gmail.com>
2022-03-15 16:16:43 +1100

    Slight improvements on mobile bruger menu style

--- src/pages/Nav.tsx
+++ src/pages/Nav.tsx
@@ -8,7 +8,7 @@ const Nav = () => {
   
     return (
         <>
-            <div className="mobile-hide">
+            <div className="mobile-hide navbar-space">
                 <Link className="navbar-link" to="/">Home</Link>
                 {auth.isAuthenticated() ? (
                     <>  
@@ -16,9 +16,6 @@ const Nav = () => {
                         <Link className="navbar-link" to="/requests">View all requests</Link>
                         <button className="btn btn-primary btn-block" onClick={()=> {
                                 auth.logout(() => {
-                                    <Navigate to="/" replace />;
-                                    //this does actually work for a single component
-                                    // update();
                                     window.location.reload();
                             })}
                         }>Sign Out</button>
@@ -30,26 +27,32 @@ const Nav = () => {
                     </>
                 )}
             </div>
-            <div className="mobile-show"> 
+            <div className="navbar-space">
+                <div className="mobile-show">
                 <Menu>
-                <Link className="navbar-link" to="/">Home</Link>
+                    <ul>
+                        <Link className="navbar-link bm-item" to="/">Home</Link>
                 {auth.isAuthenticated() ? (
                     <>  
-                        <Link className="navbar-link" to="/request">Request our service</Link>
-                        <Link className="navbar-link" to="/requests">View all requests</Link>
-                        <button onClick={()=> {
-                                auth.logout(() => {
-                                    window.location.reload();
+                        {/* instead of this single check of authetication they should render 
+                        these two tabs seperately */}
+                        <Link className="navbar-link bm-item" to="/request">Request our service</Link>
+                        <Link className="navbar-link bm-item" to="/requests">View all requests</Link>
+                        <button className="btn btn-primary btn-block bm-item" onClick={()=> {
+                            auth.logout(() => {
+                                window.location.reload();
                             })}
                         }>Sign Out</button>
                     </>
                 ) : (
                     <>
-                        <Link className="navbar-link" to="/signup">Sign Up</Link>
-                        <Link className="navbar-link" to="/login">Login</Link>
+                        <Link className="navbar-link bm-item" to="/signup">Sign Up</Link>
+                        <Link className="navbar-link bm-item" to="/login">Login</Link>
                     </>
                 )}
+                </ul>
                 </Menu>
+                </div>
             </div>
         </>
     );
