d31e639cc736b9c5f972ce79b99002047a169685
Sean Overton <sean14o8.so@gmail.com>
2022-03-22 21:32:34 +1100

    Small change in placeholder content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,9 @@ const Home = () => {
         <>
         <Nav/>
         <h1>Home page</h1>
-        <h1>Average rating: [FROM DB]/5</h1>
-        <h1>Call outs in this month: [FROM DB]</h1>
+        <h3>(Whatever information we want to put on the home page.)</h3>
+        <h3>Average rating: [FROM DB]/5</h3>
+        <h3>Call outs in this month: [FROM DB]</h3>
         <Footer/>
         </>
     );
