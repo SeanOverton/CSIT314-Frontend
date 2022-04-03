11ab46919de55e7f966c0661ff57bbd675104de9
Sean Overton <sean14o8.so@gmail.com>
2022-04-03 21:06:39 +1000

    helpers fixed

--- src/components/utils/Helpers.tsx
+++ src/components/utils/Helpers.tsx
@@ -3,5 +3,10 @@ export const formatDate = (unformatted_date: string) => {
 }
 
 export const formatTime = (unformatted_date: string) => {
+    if(!unformatted_date){
+        return unformatted_date;
+    }
+    
     return unformatted_date.split("T")[1].substring(0, 5);
+
 }
