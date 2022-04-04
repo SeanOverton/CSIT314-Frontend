fba44a9591c92790355ecc00ccc464aceae20ed7
Sean Overton <sean14o8.so@gmail.com>
2022-04-05 08:47:13 +1000

    Changed URLs before deploying

--- src/components/utils/Constants.tsx
+++ src/components/utils/Constants.tsx
@@ -1,10 +1,10 @@
 // heroku deployed django backend
-// const BACKEND_URL = "https://csit314-python-backend.herokuapp.com";
-// export const FRONTEND_URL = "https://csit314-frontend.herokuapp.com";
+const BACKEND_URL = "https://csit314-python-backend.herokuapp.com";
+export const FRONTEND_URL = "https://csit314-frontend.herokuapp.com";
 
 // for local development
-const BACKEND_URL = "http://localhost:8000";
-export const FRONTEND_URL = "http://localhost:3000";
+// const BACKEND_URL = "http://localhost:8000";
+// export const FRONTEND_URL = "http://localhost:3000";
 
 
 export default BACKEND_URL; 
