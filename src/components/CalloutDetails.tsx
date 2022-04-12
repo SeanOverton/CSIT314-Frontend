9029c72c4bbbe2ac574dcc804959f1b58174b215
Sean Overton <sean14o8.so@gmail.com>
2022-04-13 09:38:36 +1000

    Further updated the callout details page

--- src/components/CalloutDetails.tsx
+++ src/components/CalloutDetails.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import {formatDate, formatTime} from "./utils/Helpers";
 
 interface CalloutDetailsProps {
@@ -22,27 +23,94 @@ const CalloutDetails = (props: CalloutDetailsProps) => {
     
     return (
         <>
-        <h1>Customer: {details.username}</h1>
-        <h3>Location: {details.location}</h3>
+        <h3>Booking ID: {details.id}</h3>
+        <p style={{color: "grey"}}>{details.description}</p>
+        <h4>$200.00</h4>
         <h3>TODO: map here that shows location</h3>
-        {details.mechanic == "" ? (
-            <></>
-        ) : (
-            <h3>Appointed Mechanic: {details.mechanic}</h3>
-        )}
-        <h3>Date of request: {formatDate(details.date)}</h3>
-        <h3>Time of request: {formatTime(details.date)}</h3>
-        <h3>Issue : {details.description}</h3>
-        {details.review == "" ? (
-            <></>
-        ) : (
-            <h3>Customer Review: '<i>{details.review}</i>'</h3>
-        )}
-        {details.rating == "0" ? (
-            <></>
-        ) : (
-            <h3>Customer Rating: {"★".repeat(parseInt(details.rating))}{"☆".repeat(10-parseInt(details.rating))}</h3>
-        )}
+        <div style={{paddingLeft: "10%", paddingRight: "10%", paddingBottom: "2em"}}>
+            <h3 style={{textAlign: "left"}}>JOB DETAILS</h3>
+            <hr/>
+            <Row style={{fontSize: "3vw"}}>
+                <Col>
+                    <p style={{color: "grey"}}>Date and time</p>
+                </Col>
+                <Col>
+                    <p>{formatTime(details.date)} {formatDate(details.date)}</p>
+                </Col>
+            </Row>
+            <hr/>
+            <Row style={{fontSize: "3vw"}}>
+                <Col>
+                    <p style={{color: "grey"}}>Customer Information</p>
+                </Col>
+                <Col>
+                    <p>{details.username}</p>
+                </Col>
+            </Row>
+            <hr/>
+            <Row style={{fontSize: "3vw"}}>
+                <Col>
+                    <p style={{color: "grey"}}>Address</p>
+                </Col>
+                <Col>
+                    <p>{details.location}</p>
+                </Col>
+            </Row>
+            <hr/>
+            <Row style={{fontSize: "3vw"}}>
+                <Col>
+                    <p style={{color: "grey"}}>Vechile Information</p>
+                </Col>
+                <Col>
+                    <p>TODO</p>
+                </Col>
+            </Row>
+            {details.mechanic == "" ? (
+                <></>
+            ) : (
+                <>
+                    <hr/>
+                    <Row style={{fontSize: "3vw"}}>
+                        <Col>
+                            <p style={{color: "grey"}}>Appointed Mechanic</p>
+                        </Col>
+                        <Col>
+                            <p>{details.mechanic}</p>
+                        </Col>
+                    </Row>
+                </>
+            )}
+            {details.review == "" ? (
+                <></>
+            ) : (
+                <>
+                    <hr/>
+                    <Row style={{fontSize: "3vw"}}>
+                        <Col>
+                            <p style={{color: "grey"}}>Review</p>
+                        </Col>
+                        <Col>
+                            <p><i>'{details.review}'</i></p>
+                        </Col>
+                    </Row>
+                </>
+            )}
+            {details.rating == "0" ? (
+                <></>
+            ) : (
+                <>
+                    <hr/>
+                    <Row style={{fontSize: "3vw"}}>
+                        <Col>
+                            <p style={{color: "grey"}}>Rating</p>
+                        </Col>
+                        <Col>
+                        {"★".repeat(parseInt(details.rating))}{"☆".repeat(5-parseInt(details.rating))}
+                        </Col>
+                    </Row>
+                </>
+            )}
+        </div>
         </>
     );
 };
