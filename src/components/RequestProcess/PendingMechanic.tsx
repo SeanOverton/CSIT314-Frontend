da81d3b1e4408507d10e0aadf0d4117582a02962
Sean Overton <sean14o8.so@gmail.com>
2022-04-12 23:32:30 +1000

    Added spinning cog to 'pending' page

--- src/components/RequestProcess/PendingMechanic.tsx
+++ src/components/RequestProcess/PendingMechanic.tsx
@@ -1,11 +1,20 @@
 import CancelCallout from "./CancelCallout";
+import { BsFillGearFill } from "react-icons/bs";
 
 const PendingMechanic = ({request}: any) => {
     return (
-        <>
-        <h1>Please hang tight, a mechanic will accept your request shortly.</h1>
-        <CancelCallout details={request}/>
-        </>
+        <div style={{padding:"2em"}}>
+            <h1>Please hang tight, a mechanic will accept your request shortly.</h1>
+            <h2>Refresh this page to see when a mechanic accepts your order.</h2>
+            <div style={{padding: "2em"}}>
+                <BsFillGearFill 
+                style={{fontSize: "10em",
+                animation: `spin 3s linear infinite`}}/>
+            </div>
+            <div style={{padding: "2em"}}>
+                <CancelCallout details={request}/>
+            </div>
+        </div>
     );
 }
 
