8ab6fdcd2e01b3d556b99059b52392d91b4a4c39
Sean Overton <sean14o8.so@gmail.com>
2022-04-12 23:21:15 +1000

    Updated details in callout to include reviews/ratings

--- src/components/CalloutDetails.tsx
+++ src/components/CalloutDetails.tsx
@@ -22,13 +22,27 @@ const CalloutDetails = (props: CalloutDetailsProps) => {
     
     return (
         <>
-        <h1>Customer {details.username}</h1>
-        <h3>Location {details.location}</h3>
-        <h3>Mechanic {details.mechanic}</h3>
-        <h3>TODO: map here</h3>
-        <h3>Date: {formatDate(details.date)}</h3>
+        <h1>Customer: {details.username}</h1>
+        <h3>Location: {details.location}</h3>
+        <h3>TODO: map here that shows location</h3>
+        {details.mechanic == "" ? (
+            <></>
+        ) : (
+            <h3>Appointed Mechanic: {details.mechanic}</h3>
+        )}
+        <h3>Date of request: {formatDate(details.date)}</h3>
         <h3>Time of request: {formatTime(details.date)}</h3>
-        <h3>Customer Comment: {details.description}</h3>
+        <h3>Issue : {details.description}</h3>
+        {details.review == "" ? (
+            <></>
+        ) : (
+            <h3>Customer Review: '<i>{details.review}</i>'</h3>
+        )}
+        {details.rating == "0" ? (
+            <></>
+        ) : (
+            <h3>Customer Rating: {"★".repeat(parseInt(details.rating))}{"☆".repeat(10-parseInt(details.rating))}</h3>
+        )}
         </>
     );
 };
