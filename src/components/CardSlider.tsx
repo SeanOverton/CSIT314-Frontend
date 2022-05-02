6b0f50f285ffb62b8528d2aecc4b868b25b23478
Glenio <atglenio@gmail.com>
2022-05-02 14:41:58 +1000

    added profile page update, display functions

--- src/components/CardSlider.tsx
+++ src/components/CardSlider.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import "../styles/CardSlider.css";
 import car_logo from "../images/car_logo.png";
+import {BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill} from "react-icons/bs";
 
-const Card=() =>{
+
+const Card=(props:any) =>{
+    var details : any =  props.details;
     return(
         <div className="slider-card" style={{width:'300px', height:'300px'}}>
            <img src = {car_logo} style={{width:'50%', height:'50%'}}></img>
-           <h2>Honda</h2>
-           <div className='lable'>rego</div>
+           <h2>{details.vehicle_brand} {details.vehicle_model}</h2>
+           <div className='lable'>{details.vehicle_registration}</div>
            <div className='lable'>
                <label>Subscribed</label> 
                <label>24/03/2022-24/03/2024</label>
@@ -16,13 +19,30 @@ const Card=() =>{
     );
 }
 
-const CardSlider =() => {
+const CardSlider =(props:any) => {
+    const slideLeft =() => {
+        var slider = document.getElementById("slider");
+        slider!.scrollLeft = slider!.scrollLeft - 300;
+    }
 
+    const slideRight =() => {   
+        var slider = document.getElementById("slider");
+        slider!.scrollLeft = slider!.scrollLeft + 300;
+    }
     return(
         <div className="slider-wrapper">
-           <Card></Card>
-           <Card></Card>
-           <Card></Card>
+            <BsFillArrowLeftCircleFill size={40} style={{position:"absolute", left :0}} onClick={slideLeft}/>
+            <div id="slider">
+            {  
+                props.sliders.map((sub :any, index:number)=>{
+                    return(
+                        <Card details = {sub} key = {index}></Card>
+                    );
+                })
+                
+            }
+            </div>
+            <BsFillArrowRightCircleFill size={40} style={{position:"absolute", right :0}} onClick={slideRight}/>
         </div>
     );
 }
